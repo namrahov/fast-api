d2416240c879dca083cb5bdd02ded7eb5ef17838
Nurlan Amrahov <nurlan.amrahov@ailab.az>
2025-11-12 02:53:05 +0400

    Refactoring-12 db_dependency

--- controller/UserController.py
+++ controller/UserController.py
@@ -1,10 +1,9 @@
-from typing import Annotated
 from fastapi import File, Depends, status
 from main import app
-from db.database import get_session
+from db.database import get_session, db_dependency as db_depenceny
 from service.UserService import *
 
 
 @app.post("/upload-excel", status_code=status.HTTP_201_CREATED)
-async def upload_excel(file: UploadFile = File(...), db: Annotated[Session, Depends(get_session)] = None):
+async def upload_excel(file: UploadFile = File(...), db: db_depenceny = None):
     await UserService(db).upload_excel_file(file)
--- db/database.py
+++ db/database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 import os
+from typing import Annotated
+from fastapi import Depends
 
 DATABASE_URL = (
     f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@"
@@ -22,6 +24,7 @@ def get_session():
     finally:
         session.close()
 
+db_dependency = Annotated[Session, Depends(get_session)]
 
 
 
